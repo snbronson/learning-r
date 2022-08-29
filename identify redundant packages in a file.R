4625ba5c09628db2b312849f81e638a013aa32bd
Scott Bronson <16666011+snbronson@users.noreply.github.com>
2022-08-29 17:35:10 -0500

    Commit redundant packages script

---
+++ identify redundant packages in a file.R
@@ -0,0 +1,13 @@
+# FILE DESCRIPTION ############################################################
+# For a description of the purpose of this file, read the discussion at
+# https://towardsdatascience.com/r-you-sure-youre-using-this-package-8ce265a990b0)
+
+# CHECKS FOR THE NCMISC PACKAGE, AND INSTALLS IT IF NEEDED ####################
+if (!require("NCmisc")) install.packages("NCmisc")
+
+# LOAD LIBRARIES ##############################################################
+library(NCmisc)
+
+# RUN list.functions.in.file ##################################################
+p <- NCmisc::list.functions.in.file("~/path/filename.R")
+summary(p)
